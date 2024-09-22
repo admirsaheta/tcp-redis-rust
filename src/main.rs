5b3b7471feedd287c0ff4b9d1cce69da9220e0c0
admirsaheta <admirxsaheta@gmail.com>
2024-09-22 21:15:09 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,63 @@
 #![allow(unused_imports)]
-use std::net::TcpListener;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+
+trait Command {
+    fn execute(&self) -> String;
+}
+
+struct PingCommand;
+
+impl Command for PingCommand {
+    fn execute(&self) -> String {
+        "+PONG\r\n".to_string()
+    }
+}
+
+trait RedisResponse {
+    fn format_response(&self) -> String;
+}
+
+impl RedisResponse for PingCommand {
+    fn format_response(&self) -> String {
+        self.execute()
+    }
+}
+
+trait ConnectionHandler {
+    fn handle_client(&self, stream: TcpStream);
+}
+
+struct RedisConnectionHandler;
+
+impl RedisConnectionHandler {
+    fn new() -> Self {
+        RedisConnectionHandler {}
+    }
+}
+
+impl ConnectionHandler for RedisConnectionHandler {
+    fn handle_client(&self, mut stream: TcpStream) {
+        let command = PingCommand;
+        let response = command.format_response();
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+    println!("Starting Redis server on 127.0.0.1:6379...");
 
-    // Uncomment this block to pass the first stage
-    
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    
+    let handler = RedisConnectionHandler::new();
+
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                println!("Accepted new connection");
+                handler.handle_client(stream);
             }
             Err(e) => {
-                println!("error: {}", e);
+                println!("Error accepting connection: {}", e);
             }
         }
     }
