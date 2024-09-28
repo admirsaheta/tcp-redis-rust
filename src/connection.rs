65ab08d9c8b05e7380fa2c123093b385416e4915
admirsaheta <admirxsaheta@gmail.com>
2024-09-28 08:44:50 +0200

    add:key-expiry

--- src/connection.rs
+++ src/connection.rs
@@ -5,6 +5,7 @@ use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::{Duration, Instant};
 
 pub trait ConnectionHandler {
     fn handle_client(&self, stream: TcpStream);
@@ -12,18 +13,45 @@ pub trait ConnectionHandler {
 
 pub struct RedisConnectionHandler {
     store: Arc<Mutex<HashMap<String, String>>>,
+    expiry: Arc<Mutex<HashMap<String, Instant>>>,
 }
+
 impl RedisConnectionHandler {
     pub fn new() -> Self {
-        RedisConnectionHandler {
+        let handler = RedisConnectionHandler {
             store: Arc::new(Mutex::new(HashMap::new())),
-        }
+            expiry: Arc::new(Mutex::new(HashMap::new())),
+        };
+        handler.start_expiry_cleanup();
+        handler
     }
 
     pub fn handle_concurrent_clients(self: Arc<Self>, stream: TcpStream) {
         let handler = Arc::clone(&self);
         thread::spawn(move || {
             handler.handle_client(stream);
+        });
+    }
+
+    fn start_expiry_cleanup(&self) {
+        let store = Arc::clone(&self.store);
+        let expiry = Arc::clone(&self.expiry);
+
+        thread::spawn(move || loop {
+            thread::sleep(Duration::from_secs(1));
+
+            let mut expiry_map = expiry.lock().unwrap();
+            let mut store_map = store.lock().unwrap();
+
+            let mut now = Instant::now();
+            expiry_map.retain(|key, expiry_time| {
+                if expiry_time <= &mut now {
+                    store_map.remove(key);
+                    false
+                } else {
+                    true
+                }
+            });
         });
     }
 }
@@ -56,9 +84,28 @@ impl ConnectionHandler for RedisConnectionHandler {
                                 echo_command.format_response(message)
                             }
                             "SET" => {
-                                if let (Some(key), Some(value)) = (parts.next(), parts.next()) {
+                                let key = parts.next();
+                                let value = parts.next();
+                                let expiry_time = if let (Some("EX"), Some(seconds)) =
+                                    (parts.next(), parts.next())
+                                {
+                                    Some(seconds.parse::<u64>().unwrap())
+                                } else {
+                                    None
+                                };
+
+                                if let (Some(key), Some(value)) = (key, value) {
                                     let mut store = self.store.lock().unwrap();
                                     store.insert(key.to_string(), value.to_string());
+
+                                    if let Some(expiry_seconds) = expiry_time {
+                                        let mut expiry_map = self.expiry.lock().unwrap();
+                                        expiry_map.insert(
+                                            key.to_string(),
+                                            Instant::now() + Duration::from_secs(expiry_seconds),
+                                        );
+                                    }
+
                                     "+OK\r\n".to_string()
                                 } else {
                                     "-ERR wrong number of arguments for 'SET' command\r\n"
@@ -66,7 +113,8 @@ impl ConnectionHandler for RedisConnectionHandler {
                                 }
                             }
                             "GET" => {
-                                if let Some(key) = parts.next() {
+                                let key = parts.next();
+                                if let Some(key) = key {
                                     let store = self.store.lock().unwrap();
                                     if let Some(value) = store.get(key) {
                                         format!("${}\r\n{}\r\n", value.len(), value)
